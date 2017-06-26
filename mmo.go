3b656f715a10b520cec13e087f2e4d6a2d3b5a47
rysavyvladan <rysavy.vladan@gmail.com>
2017-06-26 10:30:57 +0200

    refactor(main/cli): refactored cli from switch to app.Commands

--- mmo.go
+++ mmo.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"os"
 	"github.com/urfave/cli"
 	"fmt"
-	"github.com/flowup/mmo/services"
 	"github.com/flowup/mmo/utils"
+	"os"
+	"github.com/flowup/mmo/services"
 )
 
 func main() {
@@ -13,54 +13,114 @@ func main() {
 	app.Name = "mmo"
 	app.Usage = ""
 
-	app.Action = func(c *cli.Context) error {
-		if c.NArg() == 1 {
-			return utils.ErrNoArg
-		}
+	app.Commands = []cli.Command{
+		{
+			Name:    "project",
+			Aliases: []string{},
+			Usage:   "",
+			Action: func(c *cli.Context) error {
+				if c.Args().First() == ""{
+					fmt.Println("No arguments")
+					return utils.ErrNoArg
+				}
 
-		switch (c.Args().Get(0)) {
-
-		case "project":
-			if (c.NArg() == 2) {
-				if err := services.Project(c.Args().Get(1)); err != nil {
+				if err := services.Project(c.Args().First()); err != nil {
 					fmt.Println(err)
 					return err
 				}
 				return nil
-			}
-			fmt.Println("project not implemented yet")
-			return utils.ErrNotImplemented
+			},
 
-		case "service":
-			fmt.Println("service not implemented yet")
-			return utils.ErrNotImplemented
+		},
 
-		case "set-context":
-			fmt.Println("set-context not implemented yet")
-			return utils.ErrNotImplemented
+		{
+			Name:    "service",
+			Aliases: []string{},
+			Usage:   "",
+			Action: func(c *cli.Context) error {
+				fmt.Println("add not implemented yet")
+				return utils.ErrNotImplemented
 
-		case "dev":
-			fmt.Println("dev not implemented yet")
-			return utils.ErrNotImplemented
+			},
 
-		case "build":
-			fmt.Println("build not implemented yet")
-			return utils.ErrNotImplemented
+		},
 
-		case "e2e":
-			fmt.Println("e2e not implemented yet")
-			return utils.ErrNotImplemented
+		{
+			Name:    "set-contex",
+			Aliases: []string{},
+			Usage:   "",
+			Action: func(c *cli.Context) error {
+				fmt.Println("add not implemented yet")
+				return utils.ErrNotImplemented
 
-		case "deploy":
-			fmt.Println("deploy not implemented yet")
-			return utils.ErrNotImplemented
+			},
 
-		case "add":
-			fmt.Println("add not implemented yet")
-			return utils.ErrNotImplemented
+		},
 
-		}
-		return nil
+		{
+			Name:    "dev",
+			Aliases: []string{},
+			Usage:   "",
+			Action: func(c *cli.Context) error {
+				fmt.Println("add not implemented yet")
+				return utils.ErrNotImplemented
+
+			},
+
+		},
+
+		{
+			Name:    "build",
+			Aliases: []string{},
+			Usage:   "",
+			Action: func(c *cli.Context) error {
+				fmt.Println("add not implemented yet")
+				return utils.ErrNotImplemented
+
+			},
+
+		},
+
+		{
+			Name:    "e2e",
+			Aliases: []string{},
+			Usage:   "",
+			Action: func(c *cli.Context) error {
+				fmt.Println("add not implemented yet")
+				return utils.ErrNotImplemented
+
+			},
+
+		},
+
+
+		{
+			Name:    "deploy",
+			Aliases: []string{},
+			Usage:   "",
+			Action: func(c *cli.Context) error {
+				fmt.Println("add not implemented yet")
+				return utils.ErrNotImplemented
+
+			},
+
+		},
+
+
+		{
+			Name:    "add",
+			Aliases: []string{},
+			Usage:   "",
+			Action: func(c *cli.Context) error {
+				fmt.Println("add not implemented yet")
+				return utils.ErrNotImplemented
+
+			},
+
+		},
+
+
 	}
+
 	app.Run(os.Args)
 }
