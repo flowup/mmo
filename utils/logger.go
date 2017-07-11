5a90a2dac14e768bf372c23e3a5a36c7b819b616
rysavyvladan <rysavy.vladan@gmail.com>
2017-07-11 09:56:15 +0200

    feat(Sentry): integrated sentry

---
+++ utils/logger.go
@@ -0,0 +1,36 @@
+package utils
+
+import (
+	"github.com/sirupsen/logrus"
+	"github.com/evalphobia/logrus_sentry"
+	"os"
+	"time"
+)
+
+var (
+	Log *logrus.Logger
+	dsn = "https://5554d201ce064e8790792540de39c608:fb93a01a320a486ab40b4fbb5feaf7ac@sentry.io/190135"
+)
+
+func init() {
+
+	Log = logrus.New()
+
+	// Logging format is JSON
+	Log.Formatter = &logrus.JSONFormatter{}
+
+	Log.Out = os.Stdout
+
+	levels := []logrus.Level{
+		logrus.PanicLevel,
+		logrus.FatalLevel,
+		logrus.ErrorLevel,
+	}
+	hook, err := logrus_sentry.NewSentryHook(dsn, levels)
+	hook.Timeout = 20 * time.Second
+	hook.StacktraceConfiguration.Enable = true
+
+	if err == nil {
+		Log.Hooks.Add(hook)
+	}
+}
