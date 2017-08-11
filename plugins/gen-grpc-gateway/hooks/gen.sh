b369f2fd36f313739d75272ef2965a7300db82db
Jakub Kulich <jakubkulich@gmail.com>
2017-08-11 13:24:47 +0200

    feat(plugins): added webrpc/ts protogen plugin

--- plugins/gen-grpc-gateway/hooks/gen.sh
+++ plugins/gen-grpc-gateway/hooks/gen.sh
@@ -2,7 +2,7 @@
 
 for service in "$@"
 do
-    echo "Generating gRPC server and stubs from protofile for service $service"
+    echo "Generating gRPC gateway from protofile for service $service"
     cd /source/$service/protobuf
     protoc -I/usr/local/include -I. -I/googleapis -I${GOPATH}/src --grpc-gateway_out=logtostderr=true:/source/$service proto.proto
 done
