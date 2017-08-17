e19038060a4303fc61be692d3d2e718c3b43a3c8
rysavyvladan <rysavy.vladan@gmail.com>
2017-08-17 17:54:20 +0200

    feat(plugins/interfaces): added interfaces plugin

--- plugins/gen-interfaces/intrfcs.go
+++ plugins/gen-interfaces/intrfcs.go
@@ -1,46 +1,99 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
 	"regexp"
 	"strings"
+	"os"
+	log "github.com/sirupsen/logrus"
+	"flag"
 )
 
 func main() {
-	fmt.Println("INTERFACES")
+	service := flag.String("service", "", "a name of service")
+
+	log.Info("Add service interfaces to " + *service)
+
+	if err := Parse(*service+"/proto.pb.go", *service+"/service.go"); err != nil {
+		log.Fatal(err)
+	}
 
 }
 
-func Parse(path string) (error) {
-	interfaceResult := ""
+var regInterfaces = regexp.MustCompile(`type .[^\s]*Client interface {(\s*.[^\n]*\s*[^\}]*)}`)
+var regFunc = regexp.MustCompile(`\s*(.[^\(]*)\s*\(\s*((\S*)\s*(\S*))\s*,\s*((\S*)\s*(\S*))\s*,\s*((\S*)\s*(\S*))\s*\)\s*\(((\S*)\s*,\s*(\S*))\s*\)\s*`)
 
-	content, err := ioutil.ReadFile(path)
+func Parse(inputPath, outputPath string) (error) {
+	// load content of proto.pb.go file
+	protoContent, err := ioutil.ReadFile(inputPath)
 	if err != nil {
 		return err
 	}
 
-	for i, match := range regexp.MustCompile(`type .[^\s]*Client interface {(\s*.[^\n]*\s*[^\}]*)}`).FindStringSubmatch(string(content)) {
-		if i%2 == 0 {
-			continue
-		}
+	// load content of service.go file
+	serviceContent, err := ioutil.ReadFile(outputPath)
+	if err != nil {
+		return err
+	}
 
+	// this regexp find all interfaces
+	for _, match := range regInterfaces.FindAllString(string(protoContent), -1) {
+
+		// parse line by line
 		for _, lines := range strings.Split(match, "\n") {
-			result := regexp.MustCompile(`\s*(.[^\(]*)\s*\(\s*((\S*)\s*(\S*))\s*,\s*((\S*)\s*(\S*))\s*,\s*((\S*)\s*(\S*))\s*\)\s*\(((\S*)\s*,\s*(\S*))\s*\)\s*`).FindStringSubmatch(lines)
+			result := regFunc.FindStringSubmatch(lines)
 
 			if len(result) <= 0 {
 				continue
 			}
 
-			interfaceResult += "func " + result[0][1:] + " {\n" +
-				"\n" +
-				"\treturn &"+result[7][1:]+"{}, nil\n" +
-				"}\n\n"
+			// check if is interface already at service file
+			if !regexp.MustCompile(`\s*` + regexp.QuoteMeta(result[1]) +
+				`\s*\(\s*` +
+				regexp.QuoteMeta(result[3]) +
+				`\s*` +
+				regexp.QuoteMeta(result[4]) +
+				`\s*\,\s*` +
+				regexp.QuoteMeta(result[6]) +
+				`\s*` +
+				regexp.QuoteMeta(result[7]) +
+				`\s*\)\s*\(\s*` +
+				regexp.QuoteMeta(result[12]) +
+				`\s*\,\s*` +
+				regexp.QuoteMeta(result[13])).
+				MatchString(string(serviceContent)) {
 
+				log.Info("Adding " + result[1] + "interface to service")
+
+				// open file to append interfaces
+				var file, err = os.OpenFile(outputPath, os.O_APPEND|os.O_WRONLY, 0600)
+				if err != nil {
+					return err
+				}
+				defer file.Close()
+
+				// added to file new interfaces
+				if _, err := file.WriteString("\nfunc (s *Service) " +
+					result[1] +
+					"(" +
+					result[2] +
+					", " +
+					result[5] +
+					") (" +
+					result[11] +
+					") {\n" +
+					"\n" +
+					"\treturn &" + result[12][1:] + "{}, nil\n" +
+					"}\n"); err != nil {
+					return err
+				}
+
+				if err := file.Sync(); err != nil {
+					return err
+				}
+			}
 		}
 	}
-
-	fmt.Println(interfaceResult)
 
 	return nil
 }
--- plugins/gen-interfaces/intrfcs_test.go
+++ plugins/gen-interfaces/intrfcs_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/stretchr/testify/suite"
 	"testing"
+	"github.com/flowup/mmo/utils/filecompare"
 )
 
 type IntrfcsAccessSuite struct {
@@ -17,9 +18,35 @@ func (s *IntrfcsAccessSuite) SetupTest() {
 }
 
 // Test
+type testStruct struct {
+	Input    string
+	Output   string
+	Expected string
+}
+
 func (s *IntrfcsAccessSuite) TestParse() {
-	err := Parse("fixtures/proto1_go")
-	s.Nil(err)
+	candidates := []testStruct{
+		{
+			Input:    "fixtures/proto1_go",
+			Output:   "fixtures/service1_go",
+			Expected: "fixtures/service_orig_go",
+		},
+		{
+			Input:    "fixtures/proto1_go",
+			Output:   "fixtures/service2_go",
+			Expected: "fixtures/service_orig_go",
+		},
+		{
+			Input:    "fixtures/proto1_go",
+			Output:   "fixtures/service3_go",
+			Expected: "fixtures/service_orig_go",
+		},
+	}
+
+	for _, candidate := range candidates {
+		s.Nil(Parse(candidate.Input, candidate.Output))
+		s.Nil(filecompare.CompareFiles(candidate.Output, candidate.Expected))
+	}
 }
 
 func TestIntrfcsAccessSuite(t *testing.T) {
