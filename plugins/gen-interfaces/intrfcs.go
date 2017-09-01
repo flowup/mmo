17b2382bb5f26eb2405d0af7b8a94583e596f5b1
rysavyvladan <rysavy.vladan@gmail.com>
2017-09-01 12:02:24 +0200

    refactor(plugin/interfaces): refactored test

--- plugins/gen-interfaces/intrfcs.go
+++ plugins/gen-interfaces/intrfcs.go
@@ -12,8 +12,15 @@ func main() {
 	services := os.Args[1:]
 
 	for _, service := range services {
+		outputPath := "/source/" + service + "/service.go"
+
 		log.Println("Generating service of " + service + " from interface")
-		if err := Parse("/source/"+service+"/proto.pb.go", "/source/"+service+"/service.go"); err != nil {
+		newInterfaces, err := Parse("/source/"+service+"/proto.pb.go", outputPath)
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = writeToFile(newInterfaces, outputPath)
+		if err != nil {
 			log.Fatal(err)
 		}
 	}
@@ -21,17 +28,19 @@ func main() {
 
 var regInterfaces = regexp.MustCompile(`type .[^\s]*Client interface {(\s*.[^\n]*\s*[^\}]*)}`)
 
-func Parse(inputPath, outputPath string) error {
+func Parse(inputPath, outputPath string) ([]string, error) {
+	newInterfaces := []string{}
+
 	// load content of proto.pb.go file
 	protoContent, err := ioutil.ReadFile(inputPath)
 	if err != nil {
-		return err
+		return newInterfaces, err
 	}
 
 	// load content of service.go file
 	serviceContent, err := ioutil.ReadFile(outputPath)
 	if err != nil {
-		return err
+		return newInterfaces, err
 	}
 
 	// this regexp find all interfaces
@@ -62,42 +71,48 @@ func Parse(inputPath, outputPath string) error {
 				MatchString(string(serviceContent)) {
 
 				log.Println("Adding " + result[0] + "interface to service")
-
 				// open file to append interfaces
-				var file, err = os.OpenFile(outputPath, os.O_APPEND|os.O_WRONLY, 0600)
-				if err != nil {
-					return err
-				}
-				defer file.Close()
-
-				// added to file new interfaces
-				if _, err := file.WriteString("\nfunc (s *Service) " +
-					result[0] +
-					"(" +
-					result[1] +
-					" " +
-					result[2] +
-					", " +
-					result[3] +
-					" " +
-					result[4] +
-					") (" +
-					result[7] +
-					", " +
-					result[8] +
-					") {\n" +
-					"\n" +
-					"\treturn &" + result[7][1:] + "{}, nil\n" +
-					"}\n"); err != nil {
-					return err
-				}
-
-				if err := file.Sync(); err != nil {
-					return err
-				}
+				newInterfaces = append(newInterfaces, "\nfunc (s *Service) "+
+					result[0]+
+					"("+
+					result[1]+
+					" "+
+					result[2]+
+					", "+
+					result[3]+
+					" "+
+					result[4]+
+					") ("+
+					result[7]+
+					", "+
+					result[8]+
+					") {\n"+
+					"\n"+
+					"\treturn &"+ result[7][1:]+ "{}, nil\n"+
+					"}\n", outputPath)
 			}
 		}
 	}
 
+	return newInterfaces, nil
+}
+
+func writeToFile(newInterfaces []string, outputPath string) error {
+	var file, err = os.OpenFile(outputPath, os.O_APPEND|os.O_WRONLY, 0600)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// added to file new interfaces
+	for _, newInterface := range newInterfaces {
+		if _, err := file.WriteString(newInterface); err != nil {
+			return err
+		}
+	}
+
+	if err := file.Sync(); err != nil {
+		return err
+	}
 	return nil
 }
