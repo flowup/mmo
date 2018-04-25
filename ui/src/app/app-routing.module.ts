41fba06a8c7f0f4f4a7937cfdf60ce685ef51967
kuba <jakubkulich@gmail.com>
2018-04-25 18:02:38 +0200

    fix(ui/routing): angular routing caused 404 errors on the go server

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -36,7 +36,7 @@ export const navItems: NavigationItem[] = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(navItems)],
+  imports: [RouterModule.forRoot(navItems, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
