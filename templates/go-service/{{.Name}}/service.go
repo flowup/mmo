f4bcff9da67afaa2281fc0cd100eb5fc65fedf6a
rysavyvladan <rysavy.vladan@gmail.com>
2018-07-11 10:47:38 +0200

    fix(templates): moved version to core and fixed origin

--- templates/go-service/{{.Name}}/service.go
+++ templates/go-service/{{.Name}}/service.go
@@ -15,14 +15,14 @@ func NewService() *Service {
     return &Service{}
 }
 
-func (s *Service) GetVersion(ctx context.Context, in *google_protobuf.Empty) (*Version, error) {
-    return &Version{
+func (s *Service) GetVersion(ctx context.Context, in *google_protobuf.Empty) (*core.Version, error) {
+    return &core.Version{
         Name: "1.0.0",
     }, nil
 }
 
-func (s *Service) Check(ctx context.Context, in *core.HealthCheckRequest) (*core.HealthCheckResponse, error) {
-    return &HealthCheckResponse{
+func (s *Service) Check(ctx context.Context, in *google_protobuf.Empty) (*core.HealthCheckResponse, error) {
+    return &core.HealthCheckResponse{
         Status: core.ServingStatus_SERVING,
     }, nil
 }
